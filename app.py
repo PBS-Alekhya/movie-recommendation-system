73870b59fa395503fc5e2062a73ee52b7e794c2b
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-03-07 17:22:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,59 +3,36 @@ import streamlit as st
 import requests
 import pandas as pd
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=f932f7c9f2411069e2f69b56b50fadcb&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+def get_poster(film_id):
+    api_url = f"https://api.themoviedb.org/3/movie/{film_id}?api_key=f932f7c9f2411069e2f69b56b50fadcb&language=en-US"
+    response_data = requests.get(api_url).json()
+    return f"https://image.tmdb.org/t/p/w500/{response_data.get('poster_path', '')}"
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+def suggest_movies(film_name):
+    try:
+        movie_index = movie_data.index[movie_data['title'] == film_name].tolist()[0]
+    except IndexError:
+        return [], []
+    
+    similarity_scores = [(i, score) for i, score in enumerate(similarity_matrix[movie_index])]
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)[1:6]
+    
+    suggested_movies = [(movie_data.iloc[i].title, get_poster(movie_data.iloc[i].movie_id)) for i, _ in similarity_scores]
+    suggested_titles, suggested_posters = zip(*suggested_movies) if suggested_movies else ([], [])
+    
+    return suggested_titles, suggested_posters
 
-    return recommended_movie_names, recommended_movie_posters
+st.header('AI-Powered Movie Recommender')
+movie_data = pd.DataFrame(pickle.load(open('movie_list.pkl', 'rb')))
+similarity_matrix = pickle.load(open('similarity.pkl', 'rb'))
 
+user_choice = st.selectbox("Search or choose a movie from the list", movie_data['title'].values)
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('movie_list.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
-movies = pd.DataFrame(movies)
-
-movie_lists = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_lists
-)
-
-if st.button('Recommend'):
-    recommended_movie_names, recommended_movie_posters  = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
-
-
-
-
+if st.button('Get Recommendations'):
+    suggested_titles, suggested_posters = suggest_movies(user_choice)
+    cols = st.columns(len(suggested_titles))
+    
+    for col, title, poster in zip(cols, suggested_titles, suggested_posters):
+        with col:
+            st.text(title)
+            st.image(poster)
